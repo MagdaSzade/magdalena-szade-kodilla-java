cfc064b269d12d560b05f50c1f139a968892a460
MagdaSzade <magdalenasuchanek@gmail.com>
2018-11-10 16:34:09 +0100

    Trzeci commit; moduł 6.3.

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -5,30 +5,6 @@ import com.kodilla.testing.calculator.Calculator;
 
 public class TestingMain {
     public static void main(String[] args) {
-        Calculator calculator = new Calculator();
 
-        if (calculator.add(5,2) == 7) {
-            System.out.println("Test add: OK");
-        } else {
-            System.out.println("Test add: Error!");
-        }
-
-        if (calculator.subtract(5,2) == 3) {
-            System.out.println("Test subtract: OK");
-        } else {
-            System.out.println("Test subtract: Error!");
-        }
-
-        /* moduł 6.2 testowanie klasy SimpleUser;
-        SimpleUser simpleUser = new SimpleUser("TheForumUser");
-
-        String result = simpleUser.getUsername();
-
-        if (result.equals("TheForumUser")) {
-            System.out.println("test OK");
-        } else {
-            System.out.println("Error!");
-        }
-        */
     }
 }
---
+++ kodilla-testing/src/test/java/com/kodilla/testing/collection/CollectionTestSuite.java
@@ -0,0 +1,54 @@
+package com.kodilla.testing.collection;
+
+import com.kodilla.testing.collection.OddNumbersExterminator;
+import java.util.ArrayList;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class CollectionTestSuite {
+    static int counter = 0;
+
+    @Before
+    public void before() {
+        counter += 1;
+        System.out.println("Test number " + counter + ": START");
+    }
+
+    @After
+    public void after() {
+        System.out.println("Test number " + counter + ": END");
+    }
+    @Test
+    public void testOddNumberExterminatorEmptyList() {
+        //Given
+        ArrayList<Integer> numbers = new ArrayList<>();
+        //When
+        ArrayList<Integer> oddNumbers = OddNumbersExterminator.exterminate(numbers);
+        //Then
+        Assert.assertEquals(numbers, oddNumbers);
+    }
+
+    @Test
+    public void testOddNumberExterminatorNormalList() {
+        //Given
+        ArrayList<Integer> numbers = new ArrayList<>();
+        numbers.add(3);
+        numbers.add(2);
+        numbers.add(4);
+        numbers.add(5);
+        numbers.add(8);
+
+        ArrayList<Integer> numbersOdd = new ArrayList<>();
+        numbersOdd.add(2);
+        numbersOdd.add(4);
+        numbersOdd.add(8);
+
+        //When
+        ArrayList<Integer> oddNumbers = OddNumbersExterminator.exterminate(numbers);
+        //Then
+        Assert.assertEquals(numbersOdd, oddNumbers);
+    }
+}
