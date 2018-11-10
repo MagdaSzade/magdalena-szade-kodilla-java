0d6ed646def18ae3714a97f9da619a708077b660
MagdaSzade <magdalenasuchanek@gmail.com>
2018-11-10 14:52:42 +0100

    Drugi commit moduł 6.2

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,34 @@
 package com.kodilla.testing;
 
+// import com.kodilla.testing.user.SimpleUser;
+import com.kodilla.testing.calculator.Calculator;
+
 public class TestingMain {
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie do testowania");
+        Calculator calculator = new Calculator();
+
+        if (calculator.add(5,2) == 7) {
+            System.out.println("Test add: OK");
+        } else {
+            System.out.println("Test add: Error!");
+        }
+
+        if (calculator.subtract(5,2) == 3) {
+            System.out.println("Test subtract: OK");
+        } else {
+            System.out.println("Test subtract: Error!");
+        }
+
+        /* moduł 6.2 testowanie klasy SimpleUser;
+        SimpleUser simpleUser = new SimpleUser("TheForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("TheForumUser")) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+        */
     }
 }
