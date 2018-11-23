66a688b4a2db766cca101e99c92dd13f8e913416
MagdaSzade <magdalenasuchanek@gmail.com>
2018-11-23 21:54:24 +0100

    8.2 - zadanie

---
+++ kodilla-exception/src/main/java/com/kodilla/exceptions/io/FileReader.java
@@ -0,0 +1,27 @@
+package com.kodilla.exceptions.io;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
+public class FileReader {
+
+    public void readfile() {
+
+        ClassLoader classLoader = getClass().getClassLoader();
+        File file = new File(classLoader.getResource("file/names.txt").getFile());
+        String pathString = file.getPath().replace("%20", " ");
+
+        try (Stream<String> fileLines = Files.lines(Paths.get(pathString))){
+            fileLines.forEach(System.out::println);
+        } catch (IOException e) {
+            System.out.println("Oh no! Something went wrong!" + e);
+        } finally {
+            System.out.println("I am gonna be here... alaways!");
+        }
+
+    }
+}
