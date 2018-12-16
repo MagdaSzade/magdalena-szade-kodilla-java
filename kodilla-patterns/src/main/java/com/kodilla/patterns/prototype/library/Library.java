7d017c960a396b73da94bc18b89231cd00025c40
MagdaSzade <magdalenasuchanek@gmail.com>
2018-12-16 21:55:49 +0100

    11.2 - zadanie

---
+++ kodilla-patterns/src/main/java/com/kodilla/patterns/prototype/library/Library.java
@@ -0,0 +1,45 @@
+package com.kodilla.patterns.prototype.library;
+
+import com.kodilla.patterns.prototype.Prototype;
+
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
+
+public final class Library extends Prototype {
+     private String name;
+     private Set<Book> books = new HashSet<>();
+
+    public Library(final String name) {
+        this.name = name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Set<Book> getBooks() {
+        return books;
+    }
+
+    public Library shallowCopy() throws CloneNotSupportedException {
+        return (Library) super.clone();
+    }
+
+    public Library deepCopy() throws CloneNotSupportedException {
+        Library clonedLibrary = (Library)super.clone();
+        clonedLibrary.books = new HashSet<>();
+        for(Book book : books) {
+            String clonedTitle = book.getTitle();
+            String clonedAuthor = book.getAuthor();
+            LocalDate clonedDate = book.getPublicationDate();
+            Book clonedBook = new Book(clonedTitle, clonedAuthor, clonedDate);
+            clonedLibrary.getBooks().add(clonedBook);
+        }
+        return clonedLibrary;
+    }
+}
---
+++ kodilla-patterns/src/test/java/com/kodilla/patterns/prototype/library/LibraryTestSuite.java
@@ -0,0 +1,44 @@
+package com.kodilla.patterns.prototype.library;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.time.LocalDate;
+import java.util.stream.IntStream;
+
+public class LibraryTestSuite {
+
+    @Test
+    public void testGetBooks() {
+        //Given
+        Library library = new Library("Library nr 1");
+
+        IntStream.iterate(1, n -> n + 1)
+                .limit(10)
+                .forEach(n -> library.getBooks().add(new Book("Title " + n, "Author " + n, LocalDate.of(2000, 01, n))));
+
+        Library shallowCopyLibrary = null;
+        try {
+            shallowCopyLibrary = library.shallowCopy();
+            shallowCopyLibrary.setName("Shallow copy Library");
+        } catch (CloneNotSupportedException e) {
+            System.out.println(e);
+        }
+
+        Library deepCopyLibrary = null;
+        try {
+            deepCopyLibrary = library.deepCopy();
+            deepCopyLibrary.setName("Deep copy Library");
+        } catch (CloneNotSupportedException e) {
+            System.out.println(e);
+        }
+
+        //When
+        shallowCopyLibrary.getBooks().add(new Book("Mistrz i Małgorzata", "M.Bułhakow", LocalDate.of(1989, 01,06)));
+
+        //Then
+        Assert.assertEquals(11, library.getBooks().size());
+        Assert.assertEquals(11, shallowCopyLibrary.getBooks().size());
+        Assert.assertEquals(10, deepCopyLibrary.getBooks().size());
+    }
+}
